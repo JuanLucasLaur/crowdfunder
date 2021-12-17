24ac508efafc121bcb0bfa8a955aeb7ff0191ce9
Juan L <33398520+JuanLucasLaur@users.noreply.github.com>
2021-12-17 17:43:39 -0300

    Add Campaign Factory contract

---
+++ ethereum/contracts/CampaignFactory.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+import "./Campaign.sol";
+
+/** 
+ * @title CampaignFactory
+ * @dev Allows creation and deployment of campaigns
+ */
+contract CampaignFactory {
+    Campaign[] public deployedCampaigns;
+
+    /** 
+     * @dev Create a new Campaign.
+     * @param minimum Minimum contribution allowed for the campaign.
+     */
+    function createCampaign(uint minimum) public {
+        Campaign newCampaign = new Campaign(minimum, msg.sender);
+        deployedCampaigns.push(newCampaign);
+    }
+    
+    /** 
+     * @dev Return all deployed campaigns.
+     */
+    function getDeployedCampaigns() public view returns (Campaign[] memory) {
+        return deployedCampaigns;
+    }
+}
