9593b8a039db0043e5a7ae02447881d918e01425
Juan L <33398520+JuanLucasLaur@users.noreply.github.com>
2021-12-17 17:53:03 -0300

    Add compile script

---
+++ ethereum/compile.js
@@ -0,0 +1,52 @@
+const path = require('path');
+const solc = require('solc');
+const fs = require('fs-extra');
+
+// Clean previous build.
+const buildPath = path.resolve(__dirname, 'build');
+fs.removeSync(buildPath);
+
+// Read source code.
+const campaignFactoryPath = path.resolve(__dirname, 'contracts', 'CampaignFactory.sol');
+const campaignPath = path.resolve(__dirname, 'contracts', 'Campaign.sol');
+const campaignFactorySource = fs.readFileSync(campaignFactoryPath, 'utf8');
+const campaignSource = fs.readFileSync(campaignPath, 'utf8');
+
+// Compile contracts.
+const input = {
+    language: 'Solidity',
+    sources: {
+        'CampaignFactory.sol': {
+            content: campaignFactorySource
+        },
+        'Campaign.sol': {
+            content: campaignSource
+        }
+    },
+    settings: {
+        outputSelection: {
+            '*': {
+                '*': ['abi', 'evm.bytecode']
+            }
+        }
+    }
+};
+const output = JSON.parse(solc.compile(JSON.stringify(input)));
+fs.ensureDirSync(buildPath);
+
+// Loop over output and write each contract to a different file.
+if (output.errors) {
+    output.errors.forEach((err) => {
+        throw new Error(err.formattedMessage);
+    });
+} else {
+    Object.values(output.contracts).forEach(contract => {
+        Object.entries(contract).forEach(([contractName, contractProps]) => {
+            fs.writeFileSync(
+                path.resolve(buildPath, `${contractName}.json`),
+                JSON.stringify(contractProps),
+                'utf8'
+            );
+        });
+    });
+}
